489cbbe7f254c224527212e16964b7181d39eb38
dinaolmelak <dinaolmelak@gmail.com>
2019-12-24 18:53:26 -0600

    Added PostsVC

---
+++ Collabgram/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  Collabgram
+//
+//  Created by Dinaol Melak on 12/24/19.
+//  Copyright © 2019 Dinaol Melak. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Collabgram/PostsViewController.swift
@@ -0,0 +1,47 @@
+//
+//  PostsViewController.swift
+//  Collabgram
+//
+//  Created by Dinaol Melak on 12/24/19.
+//  Copyright © 2019 Dinaol Melak. All rights reserved.
+//
+
+import UIKit
+
+class PostsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var posts = [String:Any]()
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func didTapSignOut(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        
+        
+        return cell
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
