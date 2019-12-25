3f2cc960b5af9e26117a44c7adae1e9d31f37bc3
dinaolmelak <dinaolmelak@gmail.com>
2019-12-24 23:45:54 -0600

    Added Camera Functionality

--- Collabgram/CameraViewController.swift
+++ Collabgram/CameraViewController.swift
@@ -7,16 +7,49 @@
 //
 
 import UIKit
+import AlamofireImage
 
-class CameraViewController: UIViewController {
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func onTapCancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+        
+        
+    }
+    
+    @IBAction func onTapCamera(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            picker.sourceType = .camera
+        } else{
+            picker.sourceType = .photoLibrary
+        }
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        
+        // resizing the image
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage = image.af_imageScaled(to: size)
+        
+        imageView.image = scaledImage
+        dismiss(animated: true, completion: nil)
+    }
     /*
     // MARK: - Navigation
 
