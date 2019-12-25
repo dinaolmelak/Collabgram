90d0bbfa3c04829dc26cf3dadd7701b9cd06e3b2
dinaolmelak <dinaolmelak@gmail.com>
2019-12-24 18:38:25 -0600

    Made Initial Setup of Collagram

---
+++ Collabgram/SignInViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Collabgram
+//
+//  Created by Dinaol Melak on 12/24/19.
+//  Copyright © 2019 Dinaol Melak. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func onTapSignIn(_ sender: Any) {
+        
+        
+    }
+    @IBAction func onTapSignUp(_ sender: Any) {
+        
+        
+    }
+    
+}
+
